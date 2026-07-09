9ff92d17d07dd2ef1c426d026b911656ec2c11d9
Lakshaya-stark <lakstark2104@gmail.com>
2026-07-09 23:48:32 +0530

    i am not wearing my specks and its dark

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -262,6 +262,36 @@ exports.getMonthlyPlan = async (req, res) => {
       {
         $unwind: "$startDates",
       },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: {
+          month: "$_id",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
     ]);
     res.status(200).json({
       status: "Success",
