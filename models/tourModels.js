07c7a7d107f9aacb12008886127961503f1fa8ca
Lakshaya-stark <lakstark2104@gmail.com>
2026-07-10 14:44:55 +0530

    document middleware created

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 // name, rating, price, duration, groupsize, difficulty, rating-> ratingavg, ratingQuantity, discount, summery
 
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a Name"],
       unique: true,
     },
+    slug: String,
     ratingAvg: {
       type: Number,
       default: 4.5,
@@ -63,6 +65,11 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
+});
+
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
