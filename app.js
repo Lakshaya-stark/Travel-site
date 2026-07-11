fc6e1ad7e85c6ed3b5e21407e30232680d447e16
Lakshaya-stark <lakstark2104@gmail.com>
2026-07-11 07:39:55 +0530

    refactored

--- app.js
+++ app.js
@@ -13,9 +13,25 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "Fail",
-    message: `Can't Find ${req.originalUrl} on this server....`,
+  //   res.status(404).json({
+  //     status: "Fail",
+  //     message: `Can't Find ${req.originalUrl} on this server....`,
+  //   });
+
+  const err = new Error(`Can't Find ${req.originalUrl} on This Server...`);
+  err.statusCode = 404;
+  err.status = "Failed";
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 
   next();
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("./../models/tourModels");
+const APIFeature = require("../utils/apiFeatures");
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
@@ -21,59 +22,6 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludeQuery = ["page", "sort", "limit", "fields"];
-    excludeQuery.forEach((el) => {
-      delete queryObj[el];
-    });
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      let sortQuery = this.queryString.sort.split(",").join(" ");
-
-      this.query = this.query.sort(sortQuery);
-    } else {
-      this.query = this.query.sort("-createdAt");
-    }
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(",").join(" ");
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select("-__v");
-    }
-    return this;
-  }
-
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const lim = this.queryString.limit * 1 || 100;
-    const skip = lim * (page - 1);
-
-    this.query = this.query.skip(skip).limit(lim);
-
-    return this;
-  }
-}
 
 exports.getAllTours = async (req, res) => {
   try {
