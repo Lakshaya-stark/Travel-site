de9959b3334d863b6c1e85614740cbe13d6f31a1
Lakshaya-stark <lakstark2104@gmail.com>
2026-07-11 07:04:51 +0530

    fixed unhandle routes

--- app.js
+++ app.js
@@ -12,4 +12,13 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "Fail",
+    message: `Can't Find ${req.originalUrl} on this server....`,
+  });
+
+  next();
+});
+
 module.exports = app;
